33c685a17819712d4f2ce17d1d7bac0f60bcd0e0
Alexandre <alexandre.guedes@academico.ufpb.br>
2026-01-10 13:58:14 -0300

    Adiciona lógica de produtos

---
+++ produtos.py
@@ -0,0 +1,8 @@
+class produto:
+    def __init__(self, id_bebida, marca, nome, estoque, preco, validade):
+        self.id_bebida = id_bebida
+        self.marca = marca
+        self.nome = nome
+        self.estoque = estoque
+        self.preco = preco
+        self.validade = validade
